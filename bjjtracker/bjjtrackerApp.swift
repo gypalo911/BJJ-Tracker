4b60db95cf39f439e034a2bd4df57a7719ec22c2
Petro Hupalo <petro.hupalo@eleks.com>
2023-04-05 16:22:37 +0300

    Initial Commit

---
+++ bjjtracker/bjjtrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  bjjtrackerApp.swift
+//  bjjtracker
+//
+//  Created by Petro Hupalo on 05.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct bjjtrackerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
