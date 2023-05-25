ea469b914ec7e62f174555a4f5606d07e38f943e
Petro Hupalo <petro.hupalo@eleks.com>
2023-05-25 18:38:01 +0300

    fix: Bug fix with session duration

--- bjjtracker/Views/Screens/Sessions/Activities/ActivityPanelView.swift
+++ bjjtracker/Views/Screens/Sessions/Activities/ActivityPanelView.swift
@@ -29,7 +29,7 @@ struct ActivityPanelView: View {
                                 .font(.system(size: 14))
                                 .foregroundColor(.white)
                                 .fontWeight(.bold)
-                            Text("\((session.startDate ?? Date() + TimeInterval(session.duration * 60)).toString("HH:mm"))")
+                            Text("\(((session.startDate ?? Date()) + TimeInterval(session.duration * 60)).toString("HH:mm"))")
                                 .font(.system(size: 14))
                                 .foregroundColor(.white.opacity(0.6))
                                 .fontWeight(.regular)
