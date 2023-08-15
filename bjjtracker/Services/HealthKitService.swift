ca39e0508760372fa461410a08e727768c7d75a1
Petro Hupalo <petro.hupalo@eleks.com>
2023-08-15 12:09:49 +0300

    feat: HealthKit service init

---
+++ bjjtracker/Services/HealthKitService.swift
@@ -0,0 +1,376 @@
+//
+//  HealthKitService.swift
+//  bjjtracker
+//
+//  Created by Petro Hupalo on 15.08.2023..
+//
+
+import Foundation
+import HealthKit
+
+protocol HealthKitService {
+    var isDataAuthorized: Bool { get }
+    var isMetricSystem: Bool { get }
+    func authorizeHealthKitIfNeeded(
+        completion: @escaping (Result<Bool, Error>) -> Void
+    )
+
+    func authorizationRequestStatus(
+        completion: @escaping (Result<HKAuthorizationRequestStatus, Error>) -> Void
+    )
+
+    func authorizationStatus(for type: HKObjectType) -> HKAuthorizationStatus
+    func workoutsCount(
+        dateInterval: DateInterval,
+        completion: @escaping (Result<Double?, Error>) -> Void
+    )
+    func energyStatisticsValue(
+        dateInterval: DateInterval,
+        calculation: HealthKitServiceCalculationType
+    ) async -> Double
+}
+
+enum HealthKitServiceError: Error {
+    case healthDataUnavalable
+    case dataTypeUnavailable(String)
+    case permissionsAlreadyGranted
+    case invalidDateInterval
+}
+
+extension HealthKitServiceError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .healthDataUnavalable:
+            return ""//L10n.OneApp.AppleHealth.Error.healthUnavailable
+
+        case .dataTypeUnavailable(let type):
+            return ""//L10n.OneApp.AppleHealth.Error.dataTypeUnavailable(type)
+
+        case .permissionsAlreadyGranted:
+            return ""//L10n.OneApp.AppleHealth.Error.permissionsAlreadyGranted
+        case .invalidDateInterval:
+            return ""//L10n.OneApp.AppleHealth.Error.invalidDateInterval
+        }
+    }
+}
+
+enum HealthKitServiceCalculationType {
+    case sum
+    case average
+    case max
+    case min
+    case latest
+
+    var statisticsOptions: HKStatisticsOptions {
+        switch self {
+        case .sum:
+            return .cumulativeSum
+        case .average:
+            return .discreteAverage
+        case .max:
+            return .discreteMax
+        case .min:
+            return .discreteMin
+        case .latest:
+            return .mostRecent
+        }
+    }
+    
+    var stringValue: String {
+        switch self {
+        case .sum:
+            return "Total"
+        case .average:
+            return "Daily average"
+        case .max:
+            return "Max"
+        case .min:
+            return "Min"
+        case .latest:
+            return "Latest"
+        }
+    }
+}
+
+final class DefaultHealthKitService: HealthKitService {
+
+    private let store = HKHealthStore()
+    
+    var isDataAuthorized: Bool {
+        let statuses = readTypes.map {
+            authorizationStatus(for: $0)
+        }
+        return statuses.contains(.sharingAuthorized) || !statuses.contains(.notDetermined)
+    }
+    
+    var isMetricSystem: Bool {
+        return Locale.current.usesMetricSystem
+    }
+
+    private let writeTypes: Set<HKSampleType> = Set(
+        [
+            HKSampleType.workoutType()
+        ].compactMap { $0 }
+    )
+
+    private let readTypes: Set<HKObjectType> = Set(
+        [
+            HKObjectType.workoutType(),
+            HKQuantityType.quantityType(forIdentifier: .activeEnergyBurned),
+            HKQuantityType.quantityType(forIdentifier: .basalEnergyBurned)
+        ].compactMap{ $0 }
+    )
+
+    func authorizeHealthKitIfNeeded(
+        completion: @escaping (Result<Bool, Error>) -> Void
+    ) {
+        authorizationRequestStatus { result in
+            switch result {
+            case .success(let status):
+                if status == .shouldRequest {
+                    self.authorizeHealthKit(completion: completion)
+                } else {
+                    completion(.failure(HealthKitServiceError.permissionsAlreadyGranted))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+
+    private func authorizeHealthKit(
+        completion: @escaping (Result<Bool, Error>) -> Void
+    ) {
+        guard HKHealthStore.isHealthDataAvailable() else {
+            completion(.failure(HealthKitServiceError.healthDataUnavalable))
+            return
+        }
+
+        store.requestAuthorization(toShare: writeTypes, read: readTypes) { success, error in
+            DispatchQueue.main.async {
+                if let error = error {
+                    completion(.failure(error))
+                } else {
+                    completion(.success(success))
+                }
+            }
+        }
+    }
+
+    func authorizationRequestStatus(
+        completion: @escaping (Result<HKAuthorizationRequestStatus, Error>) -> Void
+    ) {
+        store.getRequestStatusForAuthorization(
+            toShare: writeTypes,
+            read: readTypes
+        ) { status, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(status))
+            }
+        }
+    }
+
+    func authorizationStatus(for type: HKObjectType) -> HKAuthorizationStatus {
+        store.authorizationStatus(for: type)
+    }
+    
+    
+//    private func setupWorkoutsData() async -> HealthDataItemViewModel? {
+//        return await withCheckedContinuation({ continuation in
+//            self.workoutsCount(forPeriod: .today) { result in
+//                switch result {
+//                case .success(let value):
+//                    continuation.resume(returning:
+//                                            HealthDataItemViewModel(type: .workouts, value: value ?? 0)
+//                    )
+//                case .failure(_):
+//                    continuation.resume(returning: nil)
+//                }
+//            }
+//        })
+//    }
+
+//    private func setupEnergyBurnedData() async -> HealthDataItemViewModel? {
+//        let totalEnergy = await self.energyStatisticsValue(
+//            forPeriod: .today,
+//            calculation: .sum
+//        )
+//
+//        return HealthDataItemViewModel(type: .activity, value: totalEnergy)
+//    }
+}
+
+// MARK: - Fetching data
+
+extension DefaultHealthKitService {
+
+    func statisticsValue<T: HKQuantityType>(
+        quantityType: T,
+        dateInterval: DateInterval,
+        calculation: HealthKitServiceCalculationType,
+        unit: HKUnit,
+        completion: @escaping (Result<Double?, Error>) -> Void
+    ) {
+        let predicate = HKQuery.predicateForSamples(
+            withStart: dateInterval.start,
+            end: dateInterval.end,
+            options: .strictStartDate
+        )
+
+        let query = HKStatisticsQuery(
+            quantityType: quantityType,
+            quantitySamplePredicate: predicate,
+            options: calculation.statisticsOptions
+        ) { query, results, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let results = results {
+                let value: Double?
+                switch calculation {
+                case .sum:
+                    value = results.sumQuantity()?.doubleValue(for: unit)
+                case .average:
+                    value = results.averageQuantity()?.doubleValue(for: unit)
+                case .max:
+                    value = results.maximumQuantity()?.doubleValue(for: unit)
+                case .min:
+                    value = results.minimumQuantity()?.doubleValue(for: unit)
+                case .latest:
+                    value = results.mostRecentQuantity()?.doubleValue(for: unit)
+                }
+
+                completion(.success(value))
+            }
+        }
+        store.execute(query)
+    }
+    
+    func workoutsCount(
+        dateInterval: DateInterval,
+        completion: @escaping (Result<Double?, Error>) -> Void
+    ) {
+        let predicate = HKQuery.predicateForSamples(
+            withStart: dateInterval.start,
+            end: dateInterval.end,
+            options: .strictStartDate
+        )
+
+        let query = HKSampleQuery(sampleType: HKSampleType.workoutType(), predicate: predicate, limit: 60, sortDescriptors: [])
+        { query, results, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let results = results {
+                completion(.success(Double(results.count)))
+            }
+        }
+        store.execute(query)
+    }
+    
+    func statisticAverageValue(
+        quantityType: HKQuantityType?,
+        dateInterval: DateInterval,
+        calculation: HealthKitServiceCalculationType,
+        unit: HKUnit
+    ) async throws -> Double {
+        return try await withCheckedThrowingContinuation { continuation in
+            guard let quantityType = quantityType else {
+                continuation.resume(throwing: HealthKitServiceError.dataTypeUnavailable(""))
+                return
+            }
+            let predicate = HKQuery.predicateForSamples(
+                withStart: dateInterval.start,
+                end: dateInterval.end,
+                options: .strictStartDate
+            )
+            var interval = DateComponents()
+            interval.day = 1
+            
+            guard let anchorDate = Calendar.current.nextDate(
+                after: dateInterval.end,
+                matching: interval,
+                matchingPolicy: .nextTime,
+                repeatedTimePolicy: .first,
+                direction: .backward
+            ) else {
+                continuation.resume(returning: 0)
+                return
+            }
+            let query = HKStatisticsCollectionQuery(
+                quantityType: quantityType,
+                quantitySamplePredicate: predicate,
+                options: .cumulativeSum,
+                anchorDate: anchorDate,
+                intervalComponents: interval
+            )
+            
+            query.initialResultsHandler = {
+                query, results, error in
+                
+                if let error = error {
+                    continuation.resume(throwing: error)
+                    return
+                }
+                
+                guard let statsCollection = results else {
+                    continuation.resume(returning: 0)
+                    return
+                }
+                
+                var checkedDays: Set<Date> = []
+                var totalSum: Double = 0
+                
+                statsCollection.enumerateStatistics(from: dateInterval.start, to: dateInterval.end)
+                { (statistics, stop) in
+                    if let quantity = statistics.sumQuantity() {
+                        let date = statistics.endDate
+                        let value = quantity.doubleValue(for: unit)
+                        
+                        totalSum += value
+                        checkedDays.insert(date)
+                    }
+                }
+//                if !checkedDays.isEmpty {
+//                    continuation.resume(returning: totalSum / Double([.today, .yesterday].contains(period) ? 1 : checkedDays.count))
+//                } else {
+                    continuation.resume(returning: 0)
+//                }
+            }
+            store.execute(query)
+        }
+    }
+    
+    func energyStatisticsValue(
+        dateInterval: DateInterval,
+        calculation: HealthKitServiceCalculationType
+    ) async -> Double {
+        guard let activeEnergyType = HKQuantityType.quantityType(forIdentifier: .activeEnergyBurned),
+              let basalEnergyType = HKQuantityType.quantityType(forIdentifier: .basalEnergyBurned)
+        else {
+            return 0
+        }
+        return await withTaskGroup(of: Double.self, returning: Double.self) { group in
+            var totalEnergy: Double = 0
+            for energyType in [activeEnergyType, basalEnergyType] {
+                group.addTask { [self] in
+                    do {
+                        return try await statisticAverageValue(
+                            quantityType: energyType,
+                            dateInterval: dateInterval,
+                            calculation: calculation,
+                            unit: .kilocalorie()
+                        )
+                    } catch {
+                        return (0)
+                    }
+                }
+            }
+
+            for await energy in group {
+                totalEnergy += energy
+            }
+            return totalEnergy
+        }
+    }
+}
