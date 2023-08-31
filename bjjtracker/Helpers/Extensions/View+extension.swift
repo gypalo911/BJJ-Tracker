2391ab263611708aa200f43536f8f61edb5c798e
Petro Hupalo <petro.hupalo@eleks.com>
2023-08-31 11:53:33 +0300

    chore: returned important methods

--- bjjtracker/Helpers/Extensions/View+extension.swift
+++ bjjtracker/Helpers/Extensions/View+extension.swift
@@ -7,6 +7,47 @@
 
 import SwiftUI
 
+struct RoundedCorner: Shape {
+    
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+struct OffsetPreferenceKey: PreferenceKey {
+    typealias Value = CGFloat
+    static var defaultValue: Value = .zero
+
+    static func reduce(value: inout Value, nextValue: () -> Value) {
+        value = nextValue()
+    }
+}
+
+extension View {
+    @ViewBuilder
+    func offset(
+        coordinateSpace: CoordinateSpace,
+        completion: @escaping (CGFloat) -> ()
+    ) -> some View {
+        self
+            .overlay(
+                GeometryReader { proxy in
+                    let minY = proxy.frame(in: coordinateSpace).minY
+                    Color.clear
+                        .preference(key: OffsetPreferenceKey.self, value: minY)
+                        .onPreferenceChange(OffsetPreferenceKey.self) { value in
+                            completion(value)
+                        }
+                }
+            )
+    }
+}
+
+
 extension View {
     func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
         clipShape( RoundedCorner(radius: radius, corners: corners) )
@@ -30,25 +71,3 @@ extension View {
         UINavigationBar.appearance().standardAppearance = appearance
     }
 }
-
-struct RoundedCorner: Shape {
-    
-    var radius: CGFloat = .infinity
-    var corners: UIRectCorner = .allCorners
-    
-    func path(in rect: CGRect) -> Path {
-        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-        return Path(path.cgPath)
-    }
-}
-
-//extension UINavigationController: UIGestureRecognizerDelegate {
-//    override open func viewDidLoad() {
-//        super.viewDidLoad()
-//        interactivePopGestureRecognizer?.delegate = self
-//    }
-//
-//    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-//        return viewControllers.count > 1
-//    }
-//}
